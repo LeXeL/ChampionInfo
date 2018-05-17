3ea4ec186d810718353479ee826c3e5e982e1a8c
Diego Rodriguez <lexelez@gmail.com>
2018-05-17 18:48:20 -0500

    Re structure

--- src/main/index.js
+++ src/main/index.js
@@ -14,25 +14,6 @@ log.info('App starting...');
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
-let template = []
-if (process.platform === 'darwin') {
-  // OS X
-  const name = app.getName();
-  template.unshift({
-    label: name,
-    submenu: [
-      {
-        label: 'About ' + name,
-        role: 'about'
-      },
-      {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click() { app.quit(); }
-      },
-    ]
-  })
-}
 
 let win;
 
