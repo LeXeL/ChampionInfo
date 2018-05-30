743aef26efcc8c81312120e697a2224bd0c4db4c
Diego Rodriguez <lexelez@gmail.com>
2018-05-30 16:06:44 -0500

    Fixed no data on info request

--- src/renderer/components/info.vue
+++ src/renderer/components/info.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="infocomponent">
+      <v-alert :value="message" type="error">
+      There is not enough Information 
+    </v-alert>
     <v-tabs fixed-tabs dark slider-color="grey" v-model="role" >
       <v-tab v-for="(champion,index) in championsinfo" :key="index" v-cloak ripple >
         {{champion.role | role}}
@@ -34,6 +37,7 @@ export default {
             runes: [],
             items:[],
             skills:[],
+            message : false,
             mapIcon: {"8000":"Precision.png","8005":"PressTheAttack.png","8008":"FlowofBattleTemp.png","8009":"LastResortIcon.png","8010":"Conqueror.png","8014":"CoupDeGrace.png","8017":"CutDown.png","8021":"FleetFootwork.png","8100":"Domination.png","8105":"RelentlessHunter.png","8112":"Electrocute.png","8120":"GhostPoro.png","8124":"Predator.png","8126":"CheapShot.png","8128":"DarkHarvest.png","8134":"IngeniousHunter.png","8135":"RavenousHunter.png","8136":"ZombieWard.png","8138":"EyeballCollection.png","8139":"GreenTerror_TasteOfBlood.png","8143":"SuddenImpact.png","8200":"Sorcery.png","8210":"Transcendence.png","8214":"SummonAery.png","8224":"Pokeshield.png","8226":"ManaflowBand.png","8229":"ArcaneComet.png","8230":"PhaseRush.png","8232":"Waterwalking.png","8233":"AbsoluteFocus.png","8234":"CelerityTemp.png","8236":"GatheringStorm.png","8237":"Scorch.png","8242":"Unflinching.png","8243":"TheUltimateHat.png","8299":"LastStand.png","8300":"Inspiration.png","8304":"MagicalFootwear.png","8306":"HextechFlashtraption.png","8313":"PerfectTiming.png","8316":"MinionDematerializer.png","8321":"FuturesMarket.png","8326":"UnsealedSpellbook.png","8345":"BiscuitDelivery.png","8347":"CosmicInsight.png","8351":"GlacialAugment.png","8352":"TimeWarpTonic.png","8359":"Kleptomancy.png","8360":"UnsealedSpellbook.png","8400":"Resolve.png","8410":"ApproachVelocity.png","8429":"Conditioning.png","8437":"GraspOfTheUndying.png","8439":"VeteranAftershock.png","8444":"SecondWind.png","8446":"Demolish.png","8451":"Overgrowth.png","8453":"Revitalize.png","8463":"FontOfLife.png","8465":"Guardian.png","8472":"Chrysalis.png","8473":"BonePlating.png","9101":"Overheal.png","9103":"Legend_Infamy.png","9104":"Legend_Heroism.png","9105":"Legend_Tenacity.png","9111":"DangerousGame.png"}
         };
     },
@@ -168,9 +172,13 @@ export default {
             api_key: "1054371153781160d815be1fdbbc7625"
           }
         })
-        this.setRunes(data);
-        this.setItems(data);
-        this.setSkills(data);
+        if (data.body.length != 0){  
+          this.setRunes(data);
+          this.setItems(data);
+          this.setSkills(data);
+        }else{
+          this.message = true;
+        }
       } catch (e) {
         console.log(e)
       }
